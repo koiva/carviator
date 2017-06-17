2c8ac3e915fc834f505cc6c667b694a053e6772c
koiva <evgeny.shagov@cyllenex.com>
2017-06-17 16:20:53 +0200

    @CARVI-000003 Initialized module Car Ref Dms.

---
+++ car-ref-dms/src/main/java/com/carviator/car/CarRefDmsApplication.java
@@ -0,0 +1,12 @@
+package com.carviator.car;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRefDmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRefDmsApplication.class, args);
+	}
+}
