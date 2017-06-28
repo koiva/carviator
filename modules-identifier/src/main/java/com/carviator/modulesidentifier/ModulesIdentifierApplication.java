7212ac295b4c05c154d600341349b50d1b4d7126
koiva <evgeny.shagov@cyllenex.com>
2017-06-28 18:06:52 +0200

    Initial commit for Modules identifier service

---
+++ modules-identifier/src/main/java/com/carviator/modulesidentifier/ModulesIdentifierApplication.java
@@ -0,0 +1,12 @@
+package com.carviator.modulesidentifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModulesIdentifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModulesIdentifierApplication.class, args);
+	}
+}
