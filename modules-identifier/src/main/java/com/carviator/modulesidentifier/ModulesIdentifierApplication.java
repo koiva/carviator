fc7fd6f44a1deef64ea0dc1f62cb0edae40dec45
koiva <evgeny.shagov@cyllenex.com>
2017-06-28 18:23:32 +0200

    Working version of Modules Identifier service

--- modules-identifier/src/main/java/com/carviator/modulesidentifier/ModulesIdentifierApplication.java
+++ modules-identifier/src/main/java/com/carviator/modulesidentifier/ModulesIdentifierApplication.java
@@ -2,8 +2,10 @@ package com.carviator.modulesidentifier;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ModulesIdentifierApplication {
 
 	public static void main(String[] args) {
