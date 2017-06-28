f12dd8e7e23c715f164373be54d515a0c338a143
koiva <evgeny.shagov@cyllenex.com>
2017-06-28 17:58:19 +0200

    Initial commit for config provider

---
+++ config-provider/src/main/java/com/carviator/configprovider/ConfigProviderApplication.java
@@ -0,0 +1,12 @@
+package com.carviator.configprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigProviderApplication.class, args);
+	}
+}
